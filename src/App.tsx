4c5dbfe5a270ff9023d16cc5677d0741b53bf52b
Hercules3246 <mquinter90@gmail.com>
2024-05-18 15:46:29 -0500

    proyecto funcionando con funciones eliminar, agregar, propina

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-
+import MenuItem from "./components/MenuItem";
+import OrderContent from "./components/OrderContent";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+import OrderTotal from "./components/OrderTotal";
+import TipPercentageForm from "./components/TipPercentageForm";
 
 function App() {
-
+  const { order, addItem, removeItem, tip, setTip } = useOrder();
   return (
     <>
-     <h1>Hola Mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black ">
+          Calculadora de propinas y consumo
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="font-black text-4xl ">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
+        </div>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContent order={order} removeItem={removeItem} />
+          <TipPercentageForm setTip={setTip} />
+          <OrderTotal order={order} tip={tip} />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
